883351ea4bdcf08b93476703d20e7e89ae674738
MirzaAyon <ayonmirza98@gmail.com>
2022-08-18 19:11:00 +0600

    we can go one place to another place by dynamic route

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Friend = (props) => {
     const { name, username, id } = props.friend;
@@ -8,10 +8,14 @@ const Friend = (props) => {
     const showFriendDetail = () => {
         const path = `/friend/${id}`;
         navigate(path);
+        // eta direct o dewa jae abr templete string diyeo dewa jae 
+        //amra dynamic route hshebe ek ta place e jete pari link banae o korte pari
+        //abr coding er maddhomeo jete pari use navigate use kore 
     }
     return (
         <div>
             <h2>Name: {name} </h2>
+            <Link to={'/friend/' + id}>show detail</Link>
             <button onClick={showFriendDetail}>{username} id: {id}</button>
             {/* dynamic kortesi  */}
         </div>
