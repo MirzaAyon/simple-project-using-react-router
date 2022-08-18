5cee2e0ae1de4d68d8a2b1aaab751d576f1e3cc3
MirzaAyon <ayonmirza98@gmail.com>
2022-08-18 09:26:10 +0600

    initial settings done

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>lets sleep</h1>
+            <p>place for rest</p>
+        </div>
+    );
+};
+
+export default Home;
